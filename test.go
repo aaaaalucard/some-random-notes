d4842c8604b68acf28e1c4de6934d23bcb8a9ddb
aaaaalucard <aaalucard_qd@live.cn>
2024-10-22 10:45:40 -0400

    Update test.go

--- test.go
+++ test.go
@@ -11,6 +11,8 @@ import (
 func main() {
   // Use the SetServerAPIOptions() method to set the version of the Stable API on the client
   serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+  //opts := options.Client().ApplyURI("mongodb+srv://hankyuan2:D1fficultP%40ssw0rd@clusterabisf.kcskq.mongodb.net/?retryWrites=true&w=majority&appName=DedicatedCluster-1724559993").SetServerAPIOptions(serverAPI)
+  //opts := options.Client().ApplyURI("mongodb+srv://hankyuan2:P@ssw0rd@clusterabisf.kcskq.mongodb.net/?retryWrites=true&w=majority&appName=DedicatedCluster-1724559993").SetServerAPIOptions(serverAPI)
   opts := options.Client().ApplyURI("mongodb+srv://test:<db_password>@dedicatedcluster-172455.ukz8e.mongodb.net/?retryWrites=true&w=majority&appName=DedicatedCluster-1724559993").SetServerAPIOptions(serverAPI)
   //opts := options.Client().ApplyURI("mongodb+srv://asdfasdf:******@dedicatedcluster-172455.kcskq.mongodb.net/?retryWrites=true&w=majority&appName=DedicatedCluster-1724559993").SetServerAPIOptions(serverAPI)
   //opts := options.Client().ApplyURI("mongodb+srv://hankyuan:<password>@cluster0.kcskq.mongodb.net/?retryWrites=true&w=majority&appName=DedicatedCluster-1724559993").SetServerAPIOptions(serverAPI)

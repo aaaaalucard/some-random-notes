96db7dcbd4221c9e1f5fa60db6ca29538542dc0d
aaaaalucard <aaalucard_qd@live.cn>
2026-07-15 17:20:36 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://test:2LRoDf767fFAcCmy@cluster0.bzviu.mongodb-dev.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://test:2LRoDf767fFAcCmy@cluster0.dtimaqp.mongodb-dev.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
